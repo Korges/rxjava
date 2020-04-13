433600fcde84cca75776b52140353b2f479331f1
Korges <korgesik@gmail.com>
2020-04-13 15:41:39 +0200

    Add logger.

--- src/test/java/com/korges/rxjava/RxjavaApplicationTests.java
+++ src/test/java/com/korges/rxjava/RxjavaApplicationTests.java
@@ -3,13 +3,27 @@ package com.korges.rxjava;
 
 import io.reactivex.Observable;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RxjavaApplicationTests {
+
+    private final Logger logger = LoggerFactory.getLogger(RxjavaApplicationTests.class);
 
     @Test
     public void rxJava_1() {
         Observable<String> obs = Observable.just("Hello");
 
+        obs.subscribe(this::print);
+    }
+
+    @Test
+    public void rxJava_2() {
+
+    }
+
+    void print(Object obj) {
+        logger.info("Got: {}", obj);
     }
 
 }
