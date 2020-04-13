16c7556875a253a045bc486515f91100ce392cf3
Korges <korgesik@gmail.com>
2020-04-13 15:48:43 +0200

    Add Observable test with multiple values.

--- src/test/java/com/korges/rxjava/RxjavaApplicationTests.java
+++ src/test/java/com/korges/rxjava/RxjavaApplicationTests.java
@@ -10,6 +10,12 @@ public class RxjavaApplicationTests {
 
     private final Logger logger = LoggerFactory.getLogger(RxjavaApplicationTests.class);
 
+    /**
+     * Observables are the sources for the data. Usually they start providing data once a subscriber starts listening.
+     * An observable may emit any number of items (including zero items). It can terminate either successfully
+     * or with an error. Sources may never terminate, for example, an observable for a button
+     * click can potentially produce an infinite stream of events.
+     */
     @Test
     public void rxJava_1() {
         Observable<String> obs = Observable.just("Hello");
@@ -17,9 +23,14 @@ public class RxjavaApplicationTests {
         obs.subscribe(this::print);
     }
 
+    /**
+     * Observable may return multiple values
+     */
     @Test
     public void rxJava_2() {
+        Observable<String> obs = Observable.just("42", "43", "44");
 
+        obs.subscribe(this::print);
     }
 
     void print(Object obj) {
