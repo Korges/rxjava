5caf302d0cf703b413ab1877869bdfe9068dfcf4
Korges <korgesik@gmail.com>
2020-04-13 15:34:10 +0200

    Initial commit

---
+++ src/test/java/com/korges/rxjava/RxjavaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.korges.rxjava;
+
+
+import io.reactivex.Observable;
+import org.junit.Test;
+
+public class RxjavaApplicationTests {
+
+    @Test
+    public void rxJava_1() {
+        Observable<String> obs = Observable.just("Hello");
+
+    }
+
+}
